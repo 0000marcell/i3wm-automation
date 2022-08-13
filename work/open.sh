46f64daf5cdb7fdd106cd75048f237f6cd2b95cc
____marcell <0000marcell@gmail.com>
2022-08-13 15:39:46 +0000

    t push

--- work/open.sh
+++ work/open.sh
@@ -1,6 +1,11 @@
-#i3-msg 'workspace 5:Slack; exec brave https://app.slack.com/client/T042FTU5U/C02UCM45WV9'
-i3-msg 'workspace 1:Code; append_layout ./workspace-1.json'
+i3-msg 'workspace 1:Code; append_layout ./st.json'
 st -f "Liberation Mono-16" &
 sleep 1
-i3-msg 'workspace 2:Code; append_layout ./workspace-1.json'
+i3-msg 'workspace 2:Code; append_layout ./st.json'
 st -f "Liberation Mono-16" &
+sleep 1
+i3-msg 'workspace 5:Slack; append_layout ./brave.json'
+brave --new-window 'https://app.slack.com/client/T042FTU5U/G01F36FE7DF' &
+sleep 1
+i3-msg 'workspace 10:Search; append_layout ./brave.json'
+brave --new-window 'google.com' &
