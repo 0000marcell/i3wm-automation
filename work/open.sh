f1ab682201b8ce33548e6d29080065118d26b5e4
____marcell <0000marcell@gmail.com>
2022-08-14 22:41:01 +0000

    figuring out how to kill specific workspaces

--- work/open.sh
+++ work/open.sh
@@ -1,14 +1,17 @@
 i3-msg 'workspace 1:Code; append_layout ./st.json'
-st -f "Liberation Mono-16" -e bash -c 'cd ~/Decisiv/pricing' &
-sleep 5
+st -f "Liberation Mono-16" -e bash --login &
+sleep 1
 i3-msg 'workspace 2:Code; append_layout ./st.json'
-st -f "Liberation Mono-16" -e bash -c 'cd ~/Decisiv/pricing' &
-sleep 5
+st -f "Liberation Mono-16" -e bash --login &
+sleep 1
+i3-msg 'workspace 3:Code; append_layout ./st.json'
+st -f "Liberation Mono-16" -e bash -c 'irb' &
+sleep 1
 i3-msg 'workspace 4:Joplin; append_layout ./st.json'
 st -f "Liberation Mono-16" -e bash -c 'joplin' &
-sleep 2
+sleep 1
 i3-msg 'workspace 5:Slack; append_layout ./brave.json'
 brave --new-window 'https://app.slack.com/client/T042FTU5U/G01F36FE7DF' &
-sleep 2
+sleep 1
 i3-msg 'workspace 10:Search; append_layout ./brave.json'
 brave --new-window 'google.com' &
